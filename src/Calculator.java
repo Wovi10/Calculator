a623dc8ff1b40588b7e39d81e52617ed4c373b0c
Wout Vinckevleugel <wout.vinck@gmail.com>
2022-09-29 11:57:39 +0200

    Started Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Calculator {
+    private JButton NumOneBut;
+    private JButton NumTwoBut;
+    private JButton NumThreeBut;
+    private JLabel OutputLbl;
+}
