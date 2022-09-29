2790d31fc78e8bc81602989550ec6586a0dfb8d1
Wout Vinckevleugel <wout.vinck@gmail.com>
2022-09-29 12:03:41 +0200

    Started Number buttons

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,4 +5,12 @@ public class Calculator {
     private JButton NumTwoBut;
     private JButton NumThreeBut;
     private JLabel OutputLbl;
+    private JButton NumFourBut;
+    private JButton NumFiveBut;
+    private JButton NumSixBut;
+    private JButton NumSevenBut;
+    private JButton NumEightBut;
+    private JButton NumNineBut;
+    private JButton NumZeroBut;
+    private JButton NumSepBut;
 }
